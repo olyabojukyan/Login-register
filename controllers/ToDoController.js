4f27bfe20222890c7e5dbc48adac7cf7b3c2b3a6
Olya bojukyan <obojukyan@gmail.com>
2021-02-26 02:15:36 +0400

    Starting from ToDoApp

---
+++ controllers/ToDoController.js
@@ -0,0 +1,20 @@
+const { ToDo } = require("../models/toDoModel")
+
+class ToDoController {
+  async addnewToDo(req,res){
+     
+   let todo=req.body.todo
+      let result=await ToDo.create({item:todo})
+           res.json({id:result._id,item:result.item})
+   }
+   async getToDos(req,res){
+      let todos=await todo.find()
+      res.render("todo",{todos:todos})     
+   }
+   async deleteToDo(req,res){
+       let id=req.params.id
+       let result=await ToDo.deleteOne({_id:id})
+       res.json({count:result.n})
+  }
+}
+module.exports=new ToDoController()
